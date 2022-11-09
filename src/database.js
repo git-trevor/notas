99d4597e3a04379937e3d45ebc6a974480f7df35
Daniel <daniel.guerrero@outlook.com>
2022-11-09 13:08:46 -0600

    Conexión MongoDB Atlas

--- src/database.js
+++ src/database.js
@@ -1,5 +1,13 @@
 const mongose = require('mongoose');
+require('dotenv').config();
 
-mongose.connect('mongodb://localhost/notasdb')
+// const url = "mongodb+srv://notas:Notas.2022@cluster0.fl6bxba.mongodb.net/?retryWrites=true&w=majority";
+const url = process.env.MONGODB_URL
+
+mongose.connect(url)
     .then(db => console.log('Base de datos conectada'))
     .catch(err => console.log(err));
+
+// mongose.connect('mongodb://localhost/notasdb')
+//     .then(db => console.log('Base de datos conectada'))
+//     .catch(err => console.log(err));
